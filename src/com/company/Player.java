f14d96117f056d5ec32507820d1efc6273440ce1
kikeee <kikidev95@gmail.com>
2019-04-15 20:06:29 +0200

    Player osztály egy része

---
+++ src/com/company/Player.java
@@ -0,0 +1,92 @@
+package com.company;
+
+public class Player {
+    private String name;
+    private double money;
+    private String action;
+    private boolean isPlayed;
+    private String preflopAction;
+    private String flopAction;
+    private String turnAction;
+    private String riverAction;
+
+    public Player(boolean isPlayed) {
+        this.isPlayed = isPlayed;
+        this.name = "Default Name";
+        this.money = 200;
+        this.preflopAction = "No action";
+        this.flopAction = "No action";
+        this.turnAction = "No action";
+        this.riverAction = "No action";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public boolean isPlayed() {
+        return isPlayed;
+    }
+
+    public void setPlayed(boolean played) {
+        isPlayed = played;
+    }
+
+    public String getPreflopAction() {
+        return preflopAction;
+    }
+
+    public void setPreflopAction(String preflopAction) {
+        this.preflopAction = preflopAction;
+    }
+
+    public String getFlopAction() {
+        return flopAction;
+    }
+
+    public void setFlopAction(String flopAction) {
+        this.flopAction = flopAction;
+    }
+
+    public String getRiverAction() {
+        return riverAction;
+    }
+
+    public void setRiverAction(String riverAction) {
+        this.riverAction = riverAction;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", money=" + money +
+                ", action='" + action + '\'' +
+                ", isPlayed=" + isPlayed +
+                ", preflopAction='" + preflopAction + '\'' +
+                ", flopAction='" + flopAction + '\'' +
+                ", turnAction='" + turnAction + '\'' +
+                ", riverAction='" + riverAction + '\'' +
+                '}';
+    }
+}
